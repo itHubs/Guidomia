0268a3fd3a5e9699ea8c27fc78bf0990798d98ec
Pooh14 <poojakpatel14@gmail.com>
2023-01-15 13:27:14 -0500

    Initial Commit

---
+++ Guidomia/Guidomia/GuidomiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidomiaApp.swift
+//  Guidomia
+//
+//  Created by Pooja on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct GuidomiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
